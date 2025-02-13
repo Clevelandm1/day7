61b83ada0f2cf4c20a0632d17020a1351caaefd5
Cleveland Martin IV <tsarycleveland@gmail.com>
2025-02-12 18:44:26 -0600

    Added Text Below image

--- lib/cleveland.dart
+++ lib/cleveland.dart
@@ -20,6 +20,14 @@ class Cleveland extends StatelessWidget {
           children: [
             Image.network(
                 "https://img.freepik.com/premium-photo/rockstar-dog-with-fun-funky-attitude_1092689-69908.jpg"),
+                Padding(
+                  padding: const EdgeInsets.only(top: 45.0),
+                  child: Text("Nothing To See here", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text("Just a Dog with a Guitar", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w900)),
+                )
           ],
         ),
       ),
